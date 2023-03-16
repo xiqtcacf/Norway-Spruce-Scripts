84b9b6f680e12b56d33a19b8dd70c120453732d7
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2023-03-16 16:31:44 +0100

    Update AdaptiveRate.GetStatistic.recombination.R

--- 2.adaptive_rate/AdaptiveRate.GetStatistic.recombination.R
+++ 2.adaptive_rate/AdaptiveRate.GetStatistic.recombination.R
@@ -17,32 +17,14 @@ gene <- read.table('gene.scaffolds',header = T)
 recom_gene <- merge(gene,recom, by='Chr' )
 
 quantile(recom_gene$recombination_rate,probs=c(0.10))
-10% 
-0.0002902048  
 quantile(recom_gene$recombination_rate,probs=c(0.20))
-20% 
-0.0006386398  
 quantile(recom_gene$recombination_rate,probs=c(0.30))
-30% 
-0.001032138  
 quantile(recom_gene$recombination_rate,probs=c(0.40))
-40% 
-0.001474118 
 quantile(recom_gene$recombination_rate,probs=c(0.50))
-50% 
-0.001969355 
 quantile(recom_gene$recombination_rate,probs=c(0.60))
-60% 
-0.002513759  
 quantile(recom_gene$recombination_rate,probs=c(0.70))
-70% 
-0.00313483  
 quantile(recom_gene$recombination_rate,probs=c(0.80))
-80% 
-0.003981203 
 quantile(recom_gene$recombination_rate,probs=c(0.90))
-90% 
-0.005318895 
 
 bin1 <- subset(recom_gene, recom_gene$recombination_rate < 0.0002902048)
 bin2 <- subset(recom_gene, recom_gene$recombination_rate > 0.0002902048 & recom_gene$recombination_rate < 0.0006386398)
@@ -97,29 +79,6 @@ mean(bin9$recombination_rate)
 min(bin10$recombination_rate)
 max(bin10$recombination_rate)
 mean(bin10$recombination_rate)
-
-##### get bed files for each bins
-#all_scaffolds <- read.table('1k_reference_0fixed.bed',header = T)
-#bin1_bed <- merge(bin1,all_scaffolds, by='Chr')
-#bin2_bed <- merge(bin2,all_scaffolds, by='Chr')
-#bin3_bed <- merge(bin3,all_scaffolds, by='Chr')
-#bin4_bed <- merge(bin4,all_scaffolds, by='Chr')
-#bin5_bed <- merge(bin5,all_scaffolds, by='Chr')
-#bin6_bed <- merge(bin6,all_scaffolds, by='Chr')
-#bin7_bed <- merge(bin7,all_scaffolds, by='Chr')
-#bin8_bed <- merge(bin8,all_scaffolds, by='Chr')
-#bin9_bed <- merge(bin9,all_scaffolds, by='Chr')
-#bin10_bed <- merge(bin10,all_scaffolds, by='Chr')
-write.table(bin1, "bins10_recombination_bed/bin1.10.recombination.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin2, "bins10_recombination_bed/bin2.10.recombination.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin3, "bins10_recombination_bed/bin3.10.recombination.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin4, "bins10_recombination_bed/bin4.10.recombination.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin5, "bins10_recombination_bed/bin5.10.recombination.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin6, "bins10_recombination_bed/bin6.10.recombination.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin7, "bins10_recombination_bed/bin7.10.recombination.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin8, "bins10_recombination_bed/bin8.10.recombination.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin9, "bins10_recombination_bed/bin9.10.recombination.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin10, "bins10_recombination_bed/bin10.10.recombination.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
 
 ### how many 0fold / 4fold sites (all sites instead of polymorphic sites!!!!!!) and fix difference
 fold0 <- read.table('FinalUsing.0fold.all_sites.fixed_diff.bed',header = T)
@@ -176,7 +135,6 @@ sites_fold4_bin10 <- merge(fold4, bin10, by="Chr")
 sum(sites_fold0_bin10$all_sites)
 sum(sites_fold4_bin10$all_sites)
 
-
 ##### get bed files for each bins
 write.table(sites_fold0_bin1, "bins10_recombination_bed/bin1.10.recombination.divergence.0fold.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
 write.table(sites_fold4_bin1, "bins10_recombination_bed/bin1.10.recombination.divergence.4fold.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
@@ -198,13 +156,3 @@ write.table(sites_fold0_bin9, "bins10_recombination_bed/bin9.10.recombination.di
 write.table(sites_fold4_bin9, "bins10_recombination_bed/bin9.10.recombination.divergence.4fold.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
 write.table(sites_fold0_bin10, "bins10_recombination_bed/bin10.10.recombination.divergence.0fold.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
 write.table(sites_fold4_bin10, "bins10_recombination_bed/bin10.10.recombination.divergence.4fold.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-
-
-
-
-
-
-
-
-
-
