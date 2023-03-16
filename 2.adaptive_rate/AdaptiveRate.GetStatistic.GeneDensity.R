5cbee1a19ce6c03a6ae20454e3791b2cc4c8e818
XiWang <47152099+xiqtcacf@users.noreply.github.com>
2023-03-16 16:30:42 +0100

    Update AdaptiveRate.GetStatistic.GeneDensity.R

--- 2.adaptive_rate/AdaptiveRate.GetStatistic.GeneDensity.R
+++ 2.adaptive_rate/AdaptiveRate.GetStatistic.GeneDensity.R
@@ -17,33 +17,14 @@ geneD_gene <- merge(gene, geneD_gene1, by='Chr' )
 #hist(geneD_gene$gene_Den, breaks=20)
 
 quantile(geneD_gene$gene_Den,probs=c(0.10))
-10% 
-0.01862171
 quantile(geneD_gene$gene_Den,probs=c(0.20))
-20% 
-0.03564025 
 quantile(geneD_gene$gene_Den,probs=c(0.30))
-30% 
-0.05758504 
 quantile(geneD_gene$gene_Den,probs=c(0.40))
-40% 
-0.08827245
 quantile(geneD_gene$gene_Den,probs=c(0.50))
-50% 
-0.1318233 
 quantile(geneD_gene$gene_Den,probs=c(0.60))
-60% 
-0.1937714 
 quantile(geneD_gene$gene_Den,probs=c(0.70))
-70% 
-0.2844086  
 quantile(geneD_gene$gene_Den,probs=c(0.80))
-80% 
-0.4203016 
 quantile(geneD_gene$gene_Den,probs=c(0.90))
-90% 
-0.6393386 
-
 
 bin1 <- subset(geneD_gene, geneD_gene$gene_Den < 0.01862171)
 bin2 <- subset(geneD_gene, geneD_gene$gene_Den > 0.01862171 & geneD_gene$gene_Den < 0.03564025)
@@ -56,7 +37,6 @@ bin8 <- subset(geneD_gene, geneD_gene$gene_Den > 0.2844086 & geneD_gene$gene_Den
 bin9 <- subset(geneD_gene, geneD_gene$gene_Den > 0.4203016 & geneD_gene$gene_Den < 0.6393386)
 bin10 <- subset(geneD_gene, geneD_gene$gene_Den > 0.6393386)
 
-
 #### summary statistics for each bins
 sum(bin1$Num_genes)
 sum(bin2$Num_genes)
@@ -68,7 +48,6 @@ sum(bin7$Num_genes)
 sum(bin8$Num_genes)
 sum(bin9$Num_genes)
 sum(bin10$Num_genes)
-
 
 min(bin1$gene_Den)
 max(bin1$gene_Den)
@@ -100,32 +79,6 @@ mean(bin9$gene_Den)
 min(bin10$gene_Den)
 max(bin10$gene_Den)
 mean(bin10$gene_Den)
-
-
-##### get bed files for each bins
-#all_scaffolds <- read.table('1k_reference_0fixed.bed',header = T)
-#bin1_bed <- merge(bin1,all_scaffolds, by='Chr')
-#bin2_bed <- merge(bin2,all_scaffolds, by='Chr')
-#bin3_bed <- merge(bin3,all_scaffolds, by='Chr')
-#bin4_bed <- merge(bin4,all_scaffolds, by='Chr')
-#bin5_bed <- merge(bin5,all_scaffolds, by='Chr')
-#bin6_bed <- merge(bin6,all_scaffolds, by='Chr')
-#bin7_bed <- merge(bin7,all_scaffolds, by='Chr')
-#bin8_bed <- merge(bin8,all_scaffolds, by='Chr')
-#bin9_bed <- merge(bin9,all_scaffolds, by='Chr')
-#bin10_bed <- merge(bin10,all_scaffolds, by='Chr')
-
-write.table(bin1, "bins10_gene_bed/bin1.10.gene_density.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin2, "bins10_gene_bed/bin2.10.gene_density.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin3, "bins10_gene_bed/bin3.10.gene_density.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin4, "bins10_gene_bed/bin4.10.gene_density.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin5, "bins10_gene_bed/bin5.10.gene_density.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin6, "bins10_gene_bed/bin6.10.gene_density.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin7, "bins10_gene_bed/bin7.10.gene_density.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin8, "bins10_gene_bed/bin8.10.gene_density.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin9, "bins10_gene_bed/bin9.10.gene_density.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-write.table(bin10, "bins10_gene_bed/bin10.10.gene_density.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-
 
 ### how many 0fold / 4fold sites (all sites instead of polymorphic sites!!!!!!) and all fix difference
 fold0 <- read.table('FinalUsing.0fold.all_sites.fixed_diff.bed',header = T)
@@ -203,6 +156,3 @@ write.table(sites_fold0_bin9, "bins10_gene_bed//bin9.10.gene_density.divergence.
 write.table(sites_fold4_bin9, "bins10_gene_bed//bin9.10.gene_density.divergence.4fold.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
 write.table(sites_fold0_bin10, "bins10_gene_bed//bin10.10.gene_density.divergence.0fold.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
 write.table(sites_fold4_bin10, "bins10_gene_bed//bin10.10.gene_density.divergence.4fold.bed", sep="\t", quote=FALSE, col.names = T, row.names = FALSE)
-
-
-
